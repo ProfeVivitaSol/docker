148aab2afb58509b8955b113954cf501bc19cfc0
profevivitasol <profevivitasol@gmail.com>
2026-05-23 22:46:40 -0400

    feat: optimiza webclient

--- servicioreservas - Version WebCliente/src/main/java/com/clubdeportivo2/servicioreservas/services/ReservaServicesImpl.java
+++ servicioreservas - Version WebCliente/src/main/java/com/clubdeportivo2/servicioreservas/services/ReservaServicesImpl.java
@@ -1,12 +1,15 @@
 package com.clubdeportivo2.servicioreservas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-
+import com.clubdeportivo2.servicioreservas.exception.CanchaNotFoundException;
 import com.clubdeportivo2.servicioreservas.model.Reserva;
 import com.clubdeportivo2.servicioreservas.model.dto.Cancha;
 import com.clubdeportivo2.servicioreservas.repository.ReservaRepository;
+
+import reactor.core.publisher.Mono;
 import java.util.List;
 
 @Service
@@ -31,19 +34,32 @@ public class ReservaServicesImpl implements ReservaServices {
 
     @Override
     public Reserva crearReserva(Reserva reserva) {
-
         Cancha cancha = webClient
                 .get()
                 .uri("/api/canchas/" + reserva.getCanchaId())
                 .retrieve()
+                .onStatus(
+                status -> status.value() == 404,
+                response -> Mono.error(
+                    new CanchaNotFoundException("La cancha con ID " + reserva.getCanchaId() + " no existe")
+                    )
+                )
+                .onStatus(
+                HttpStatusCode::is5xxServerError,
+                response -> Mono.error(
+                    new RuntimeException("El servicio de canchas no está disponible")
+                    )
+                )
+                
                 .bodyToMono(Cancha.class)
                 .block();
-
+     
         if (cancha == null) {
-            throw new RuntimeException("La cancha no existe");
+           throw new CanchaNotFoundException("La cancha con ID " + reserva.getCanchaId() + " no existe");
         }
 
-        return reservaRepository.save(reserva);
+       return reservaRepository.save(reserva); 
+
     }
 
 
